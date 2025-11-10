be0de883123e9bfcf3a05b47334d899d58099e0e
Felipe OG <felipeog476@gmail.com>
2025-11-10 00:33:39 +0000

    fix form field

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 const mobileNavBtn = document.getElementById("mobile-nav-btn");
 const mobileNav = document.getElementById("mobile-nav");
 
-mobileNavBtn.addEventListener("click", function () {
+mobileNavBtn.addEventListener("click", () => {
   if (mobileNav.hasAttribute("class")) {
     mobileNav.removeAttribute("class");
   } else {
     mobileNav.setAttribute("class", "active");
   }
+});
+
+const inputElements = document.querySelectorAll("input");
+
+inputElements.forEach((element) => {
+  element.value = "";
+});
+
+const formGroupElements = document.querySelectorAll(".form-group input");
+
+formGroupElements.forEach((element) => {
+  element.addEventListener("blur", (event) => {
+    if (!event.target.value) {
+      event.target.classList.remove("has-value");
+    } else {
+      event.target.classList.add("has-value");
+    }
+  });
 });
